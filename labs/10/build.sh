43b7fe976a700f0f104366d6480c94a9a1a2bc29
wf <willfreyman@gmail.com>
2016-03-18 16:55:03 -0700

    started l 10

---
+++ labs/10/build.sh
@@ -0,0 +1,13 @@
+file="lab10"
+pdflatex $file.tex
+bibtex $file
+pdflatex $file.tex
+pdflatex $file.tex
+rm $file.bib
+rm $file.aux
+rm $file.bcf
+rm $file.log
+rm $file.out
+rm $file.run.xml
+rm $file.bbl
+rm $file.blg
